67ea736d62a99dbe96e376a595278b48e5251a46
Mathieu Lecarme <mlecarme@bearstech.com>
2016-03-04 21:37:10 +0100

    Show hsperf and proc infos.

--- main.go
+++ main.go
@@ -7,19 +7,29 @@ import (
 	//"strconv"
 	//"time"
 	"./hsperf"
+	"./java"
+	"./proc"
 )
 
 func main() {
 	user := os.Args[1]
-	hsPerfDataPath, err := hsperf.FindHSPerfData(user)
+	data, err := java.FindAppData(user)
 	if err != nil {
 		log.Fatal(err)
 	}
-	hsperfdata, err := hsperf.ReadHsperf(hsPerfDataPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(hsperfdata.Heap.Used, hsperfdata.Heap.Capacity, float64(100*hsperfdata.Heap.Used)/float64(hsperfdata.Heap.Capacity))
+	hsperfdata, err := hsperf.ReadHsperf(data.Path())
+	if err != nil {
+		log.Fatal(err)
+	}
+	proc, err := proc.ReadStat(data.Pid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Heap used, capacity, ratio : ", hsperfdata.Heap.Used, hsperfdata.Heap.Capacity, float64(100*hsperfdata.Heap.Used)/float64(hsperfdata.Heap.Capacity))
+	fmt.Println("CPU time User System : ", proc.Utime, proc.Stime)
 	//interval, err := strconv.Atoi(os.Args[2])
 	//if err != nil {
 	//log.Fatal(err)
